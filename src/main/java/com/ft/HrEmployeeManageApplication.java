6084dcdebda8d79d865410b4f5fb6a947add03b3
Athul <athulkrishnapv6@gmail.com>
2025-01-30 05:52:17 +0530

    first commit

---
+++ src/main/java/com/ft/HrEmployeeManageApplication.java
@@ -0,0 +1,13 @@
+package com.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEmployeeManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEmployeeManageApplication.class, args);
+	}
+
+}
