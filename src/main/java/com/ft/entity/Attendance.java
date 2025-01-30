67a8f7620dec95238e59c7a50b942448e7de51bb
AthulKrishna2k <intern.athul@flytxt.com>
2025-01-30 07:10:57 +0530

    entity done

---
+++ src/main/java/com/ft/entity/Attendance.java
@@ -0,0 +1,54 @@
+package com.ft.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Attendance {
+	@Id
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "employeeId")
+	private Employee employee;
+
+	private LocalDateTime checkIn;
+	private LocalDateTime checkOut;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public LocalDateTime getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(LocalDateTime checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public LocalDateTime getCheckOut() {
+		return checkOut;
+	}
+
+	public void setCheckOut(LocalDateTime checkOut) {
+		this.checkOut = checkOut;
+	}
+
+}
